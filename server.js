4dbb724e22f3177f3c885fdf2be4d493819110b8
Phyllis Wong <wong.phyllism@gmail.com>
2018-09-27 18:23:20 -0700

    add dependencies

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
 const exp = require('express');
 const app = exp();
+require('dotenv').config();
 const mongoose = require('mongoose');
+
+const passport = require('passport'); // Authentication
+const flash = require('connect-flash'); // messages
+
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
 
 // Port
-const port = 3001;
+const port = process.env.PORT;
 
+// configuration ===============================================================
+let mongoUri = process.env.MONGODB_URI;
+mongoose.connect(mongoUri, { useNewUrlParser: true } ); //connect our database
+mongoose.set('debug', true);
 
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.send('Hello Keto!');
 });
 
 
