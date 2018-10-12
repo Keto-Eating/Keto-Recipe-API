ab99fe0d35f51a4b826de90b40916f15108b5d84
Nicolai Safai <nicolaisafai@gmail.com>
2018-10-12 13:08:08 -0700

    updated all indentation to adhere to airbnb style guide

--- controllers/users.js
+++ controllers/users.js
@@ -15,15 +15,24 @@ module.exports = (app) => {
     const user = new UserSchema(req.body);
 
     user.save().then((user) => {
-      const token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: '60 days' });
-      res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
+      const token = jwt.sign({
+        _id: user._id
+      }, process.env.SECRET, {
+        expiresIn: '60 days'
+      });
+      res.cookie('nToken', token, {
+        maxAge: 900000,
+        httpOnly: true
+      });
       app.locals.username = req.body.username;
       // console.log(req.body.username);
       console.log(res.locals.username);
       res.redirect('/');
       // res.send("blah")
     }).catch((err) => {
-      return res.status(400).send({ err });
+      return res.status(400).send({
+        err
+      });
     });
   });
 
@@ -38,26 +47,37 @@ module.exports = (app) => {
     const password = req.body.password;
 
     // Look for this user name
-    UserSchema.findOne({ username }, 'username password')
+    UserSchema.findOne({
+        username
+      }, 'username password')
       .then((user) => {
         if (!user) {
           // User not found
-          return res.status(401).send({ message: 'Wrong Username or Password' });
+          return res.status(401).send({
+            message: 'Wrong Username or Password'
+          });
         }
 
         // Check the password
         user.comparePassword(password, (err, isMatch) => {
           if (!isMatch) {
-            return res.status(401).send({ message: 'Wrong Username or Password' });
+            return res.status(401).send({
+              message: 'Wrong Username or Password'
+            });
           }
 
           // Create the token
-          const token = jwt.sign(
-            { _id: user._id, username: user.username }, process.env.SECRET,
-            { expiresIn: '60 days' }
-          );
+          const token = jwt.sign({
+            _id: user._id,
+            username: user.username
+          }, process.env.SECRET, {
+            expiresIn: '60 days'
+          });
           // Set a cookie and redirect to root
-          res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
+          res.cookie('nToken', token, {
+            maxAge: 900000,
+            httpOnly: true
+          });
           console.log('Successfully logged in.');
           res.redirect('logged-in/');
         });
--- tests/test.js
+++ tests/test.js
@@ -10,36 +10,39 @@ chai.use(chaiHttp);
 const agent = chai.request.agent(server);
 
 describe('Users', () => {
-// Sign-up
+  // Sign-up
   it('should signup a single user POST', (done) => {
     agent.post('/sign-up')
-      .send({ username: 'test2@test.com', password: 'test123' })
+      .send({
+        username: 'test2@test.com',
+        password: 'test123'
+      })
       .end((err, res) => {
         expect(agent).to.have.cookie('nToken');
         done();
       });
   });
-// Login
-// agent
-//   .post('/session')
-//   .send({ username: 'test2@test.com', password: 'test123' })
-//   .then(function (res) {
-//     expect(res).to.have.cookie('sessionid');
-//     // The `agent` now has the sessionid cookie saved, and will send it
-//     // back to the server in the next request:
-//     return agent.get('/')
-//       .then(function (res) {
-//         expect(res).to.have.status(200);
-//       });
-//   });
-//
-// chai.request(server)
-//   .put('/login')
-//   .send({ username: 'test1@test.com', password: 'test123', confirmPassword: 'test123' })
-//   .then(function (res) {
-//     expect(res).to.have.status(200);
-//   })
-//   .catch(function (err) {
-//     throw err;
-//   });
+  // Login
+  // agent
+  //   .post('/session')
+  //   .send({ username: 'test2@test.com', password: 'test123' })
+  //   .then(function (res) {
+  //     expect(res).to.have.cookie('sessionid');
+  //     // The `agent` now has the sessionid cookie saved, and will send it
+  //     // back to the server in the next request:
+  //     return agent.get('/')
+  //       .then(function (res) {
+  //         expect(res).to.have.status(200);
+  //       });
+  //   });
+  //
+  // chai.request(server)
+  //   .put('/login')
+  //   .send({ username: 'test1@test.com', password: 'test123', confirmPassword: 'test123' })
+  //   .then(function (res) {
+  //     expect(res).to.have.status(200);
+  //   })
+  //   .catch(function (err) {
+  //     throw err;
+  //   });
 });
