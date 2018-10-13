ae55e8f96e26e9542248ac0719a8c7cab5e10692
Phyllis Wong <wong.phyllism@gmail.com>
2018-10-13 14:23:05 -0700

    Update index page to load common recipes

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ mongoose.set('debug', true);
 
 
 // routes =============================================================================
- // load our routes and pass to our app
+// load our routes and pass to our app
 require('./controllers/users')(app); // load our routes and pass to our app
 require('./controllers/favorites')(app); // load our routes and pass to our app
 
@@ -59,29 +59,33 @@ require('./controllers/favorites')(app); // load our routes and pass to our app
 // });
 
 app.get('/', (req, res) => {
-  // console.log(req.query);
-  // console.log(req.query.term)
-  const queryString = `keto ${req.query.term}`;
+  let queryString = '';
+
+  if (!req.query.term) {
+    queryString = 'keto';
+  } else {
+    queryString = `keto ${req.query.term}`;
+  }
+
   // ENCODE THE QUERY STRING TO REMOVE WHITE SPACES AND RESTRICTED CHARACTERS
   const term = encodeURIComponent(queryString);
   // PUT THE SEARCH TERM INTO THE EDEMAM API SEARCH URL
   const url = `https://api.edamam.com/search?q=${term}&app_id=${EDAMAM_APP_ID}&app_key=${EDAMAM_API_KEY}`;
-  
+
   // console.log(`URL: ${url}`);
   http.get(url, (response) => {
     response.setEncoding('utf8');
     let body = '';
 
     response.on('data', (d) => {
-      // CONTINUOUSLY UPDATE STREAM WITH DATA FROM EDEMAM
+      // CONTINUOUSLY UPDATE STREAM WITH DATA FROM EDEMAM API
       body += d;
     });
 
     response.on('end', () => {
       // WHEN DATA IS FULLY RECEIVED PARSE INTO JSON
       const parsed = JSON.parse(body);
-      // console.log(parsed.hits[0].recipe.image); // <-- Confirmed this shows the correct data!!!! -->
-      //  Index Template & pass recipe data into the template
+      // Index Template & pass recipe data to the template
       res.render('index', { recipes: parsed.hits });
     });
   });
