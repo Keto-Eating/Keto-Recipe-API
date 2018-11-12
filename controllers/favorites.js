5bcdc4f957b566355f4c8d4491f0ec5155bd6ab0
Nicolai Safai <nicolaisafai@gmail.com>
2018-11-12 13:22:46 -0800

    merging in the fix for /favorites

--- controllers/favorites.js
+++ controllers/favorites.js
@@ -1,22 +1,5 @@
 module.exports = (app) => {
   // const auth = require('./helpers/auth')
-<<<<<<< HEAD
-  const FavoriteSchema = require('../models/favorite');
-  
-  // Show all recipes saved in their cart
-  app.get('/favorites', (req, res) => {
-    console.log(app.locals.username);
-    FavoriteSchema.find({}, function(err, favorites) {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log(favorites);
-        res.render('favorites', {
-          favorites: favorites
-        });
-      }
-    });
-=======
   const RecipeSchema = require('../models/recipe');
   const UserSchema = require('../models/user');
 
@@ -37,23 +20,10 @@ module.exports = (app) => {
         console.log(recipes);
       }
     })
->>>>>>> favorites
   });
 
   // Send a POST request to the database to create the recipes collection
   app.post('/favorites/', (req, res) => {
-<<<<<<< HEAD
-    const favorite = new FavoriteSchema({
-      recipeName: req.body.recipeName,
-      imageUrl: req.body.imageUrl,
-      recipeUrl: req.body.recipeUrl,
-      ingredients: req.body.ingredients,
-      uri: req.body.uri
-    });
-    favorite.save(function(err, favorite) {
-      if (err) console.log(err);
-      console.log("successfully saved a favorite");
-=======
     const favoriteId = req.body.favoriteId;
     const userId = app.locals.user.id
 
@@ -68,7 +38,6 @@ module.exports = (app) => {
       if (err) return handleError(err);
       app.locals.user = user // update user locally
       console.log(user);
->>>>>>> favorites
     });
   });
 };
