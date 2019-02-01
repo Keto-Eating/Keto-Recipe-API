1beefdf0f18fcefad12159d9d88339b879a05f09
Phyllis Wong <wong.phyllism@gmail.com>
2019-01-31 18:01:08 -0800

    Users can remove recipes from their cart

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,34 +1,33 @@
+/* eslint-disable global-require */
+/* eslint-disable prefer-destructuring */
 module.exports = (app) => {
   const http = require('https');
-  const EDAMAM_APP_ID = process.env.EDAMAM_APP_ID;
-  const EDAMAM_API_KEY = process.env.EDAMAM_API_KEY;
   const schedule = require('node-schedule');
   const RecipeSchema = require('../models/recipe');
-	const UserSchema = require('../models/user');
+  const EDAMAM_APP_ID = process.env.EDAMAM_APP_ID;
+  const EDAMAM_API_KEY = process.env.EDAMAM_API_KEY;
 
   pullEdamamRecipes(); // do this once when server boots up
-  const edamamJob = schedule.scheduleJob('59 59 23 * * *', function() {
+  const edamamJob = schedule.scheduleJob('59 59 23 * * *', () => {
     // schedule.scheduleJob(second min hr dayOfMonth month dayOfWeek)
     pullEdamamRecipes();
   });
 
   app.get('/', (req, res) => {
-    let queryString = req.query.term;
-    var regExpQuery = new RegExp(queryString, 'i');
+    const queryString = req.query.term;
+    const regExpQuery = new RegExp(queryString, 'i');
 
     RecipeSchema.find({
       label: regExpQuery
-    }, function(err, recipes) {
-      console.log('***********************: call back')
+    }, (err, recipes) => {
+      console.log('***********************: call back');
       if (err) {
-        console.error(err.message)
+        console.error(err.message);
       } else {
-        res.render('index', {
-          recipes: recipes
-        });
+        res.render('index', { recipes });
       }
-    })
-  })
+    });
+  });
 
   function pullEdamamRecipes() {
     // TODO: add loop later to change from/to params + add max (currently 525 keto recipes)
@@ -42,22 +41,22 @@ module.exports = (app) => {
       response.on('end', () => {
         const parsed = JSON.parse(body);
 
-        parsed.hits.forEach(function(hit) {
+        parsed.hits.forEach((hit) => {
           const recipeFromAPI = new RecipeSchema(hit.recipe);
 
           RecipeSchema.findOne({ uri: hit.recipe.uri })
-            .exec(function(err, recipeInDB) {
+            .exec((err, recipeInDB) => {
               if (err) {
-                console.log('Error in recipe save: ', err.message)
+                console.log('Error in recipe save: ', err.message);
               } else if (recipeInDB) {
                 // console.log("recipe already exists");
               } else {
                 // recipe is not in DB yet, save it
                 recipeFromAPI.save((err, recipe) => {
                   if (err) {
-                    console.log('Error in recipe save: ', err.message)
+                    console.log('Error in recipe save: ', err.message);
                   } else {
-                    console.log(`successfully saved a recipe: ${ recipe.label }`)
+                    console.log(`successfully saved a recipe: ${recipe.label}`);
                   }
                 });
               }
@@ -68,23 +67,22 @@ module.exports = (app) => {
   }
 
   app.get('/', (req, res) => {
-    let queryString = req.query.term;
-    var regExpQuery = new RegExp(queryString, 'i');
+    const queryString = req.query.term;
+    const regExpQuery = new RegExp(queryString, 'i');
 
-    RecipeSchema.find({ $or:
+    RecipeSchema.find({
+      $or:
         [
           { label: regExpQuery },
           { url: regExpQuery },
           { ingredientLines: regExpQuery }
         ]
-    }, function(err, recipes) {
+    }, (err, recipes) => {
       if (err) {
-        console.error(err.message)
+        console.error(err.message);
       } else {
-        res.render('index', {
-          recipes: recipes
-        });
+        res.render('index', { recipes });
       }
-    })
-  })
-}
+    });
+  });
+};
