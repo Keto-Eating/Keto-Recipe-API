7b156252c4f99127ed3f589c7fddd2c9770bfd34
Nicolai Safai <nicolaisafai@gmail.com>
2019-04-25 18:50:14 -0700

    making progress on fix

--- controllers/users.js
+++ controllers/users.js
@@ -8,7 +8,7 @@
 /* eslint-disable global-require */
 
 module.exports = (app) => {
-  const jwt = require('jsonwebtoken');
+  // const jwt = require('jsonwebtoken');
   const UserSchema = require('../models/user');
 
   // Render the signup form
@@ -21,25 +21,28 @@ module.exports = (app) => {
     // CREATE User and JWT
     const user = new UserSchema(req.body);
 
-    user.save().then((savedUser) => {
-      const token = jwt.sign({
-        _id: savedUser._id,
-      }, process.env.SECRET, {
-        expiresIn: '60 days',
+    user
+      .save()
+      .then((savedUser) => {
+        req.session.user = savedUser;
+        res.redirect('/');
+      // const token = jwt.sign({
+      //   _id: savedUser._id,
+      // }, process.env.SECRET, {
+      //   expiresIn: '60 days',
+      // });
+      // res.cookie('nToken', token, {
+      //   maxAge: 900000,
+      //   httpOnly: true,
+      // });
+      // app.locals.user = user;
+      // res.redirect('/');
+      })
+      .catch(() => {
+        const nextError = new Error('Email address already taken. Did you mean to login?');
+        nextError.status = 422; // validation error
+        return next(nextError);
       });
-      res.cookie('nToken', token, {
-        maxAge: 900000,
-        httpOnly: true,
-      });
-      console.log(`user is: ${user}`);
-      app.locals.user = user;
-      res.redirect('/');
-      // res.send("blah")
-    }).catch(() => {
-      const nextError = new Error('Email address already taken. Did you mean to login?');
-      nextError.status = 422; // validation error
-      return next(nextError);
-    });
   });
 
   // LOGIN FORM
@@ -52,53 +55,68 @@ module.exports = (app) => {
     const username = req.body.username;
     const password = req.body.password;
 
-    // Look for this user name
-    UserSchema.findOne({
-      username,
-    }, 'username password arrayOfFavoriteRecipes')
-      .then((user) => {
-        if (!user) {
-          // User not found
-          const nextError = new Error('User with that username does not exist');
-          nextError.status = 401;
-          return next(nextError);
-        } else {
-          // console.log(user)
-          // console.log('app locals user: ' + app.locals.user);
-          // Check the password
-          user.comparePassword(password, (err, isMatch) => {
-            console.log(isMatch);
-            if (!isMatch) {
-              const nextError = new Error('Incorrect password');
-              nextError.status = 401;
-              return next(nextError);
-            } else {
-              app.locals.user = user;
-              // Create the token
-              const token = jwt.sign({
-                _id: user._id,
-                username: user.username,
-              }, process.env.SECRET, {
-                expiresIn: '60 days',
-              });
-              // Set a cookie and redirect to root
-              res.cookie('nToken', token, {
-                maxAge: 900000,
-                httpOnly: true,
-              });
-              console.log('Successfully logged in.');
-              res.redirect('/dashboard');
-            }
-          });
-        }
-      })
-      .catch(err => next(err));
+    UserSchema.authenticate(username, password, (err, user) => {
+      if (err || !user) {
+        const nextError = new Error('Email or password incorrect')
+        nextError.status = 401;
+        return next(nextError);
+      }
+      // user authenticated correctly
+      req.session.user = user;
+      return res.redirect('/lists');
+    });
+
+    // // Look for this user name
+    // UserSchema.findOne({
+    //   username,
+    // }, 'username password arrayOfFavoriteRecipes')
+    //   .then((user) => {
+    //     if (!user) {
+    //       // User not found
+    //       const nextError = new Error('User with that username does not exist');
+    //       nextError.status = 401;
+    //       return next(nextError);
+    //     } else {
+    //       // Check the password
+    //       user.comparePassword(password, (err, isMatch) => {
+    //         console.log(isMatch);
+    //         if (!isMatch) {
+    //           const nextError = new Error('Incorrect password');
+    //           nextError.status = 401;
+    //           return next(nextError);
+    //         } else {
+    //           app.locals.user = user;
+    //           // Create the token
+    //           const token = jwt.sign({
+    //             _id: user._id,
+    //             username: user.username,
+    //           }, process.env.SECRET, {
+    //             expiresIn: '60 days',
+    //           });
+    //           // Set a cookie and redirect to root
+    //           res.cookie('nToken', token, {
+    //             maxAge: 900000,
+    //             httpOnly: true,
+    //           });
+    //           console.log('Successfully logged in.');
+    //           res.redirect('/dashboard');
+    //         }
+    //       });
+    //     }
+    //   })
+    //   .catch(err => next(err));
   });
 
   // LOGOUT
-  app.get('/logout', (req, res) => {
-    res.clearCookie('nToken');
-    app.locals.user = null;
-    res.redirect('back'); // to automatically redirect back to the page the request came from
+  app.get('/logout', (req, res, next) => {
+    if (req.session) {
+      req.session.destroy((err) => {
+        if (err) return next(err);
+      });
+    }
+    // redirect back to the page the request came from
+    res.redirect('back');
+    // res.clearCookie('nToken');
+    // app.locals.user = null;
   });
 };
--- models/user.js
+++ models/user.js
@@ -2,49 +2,72 @@
 /* eslint-disable key-spacing */
 /* eslint-disable func-names */
 /* eslint-disable consistent-return */
+/* eslint-disable no-use-before-define */
 const mongoose = require('mongoose');
 
 const bcrypt = require('bcryptjs');
 const uniqueValidator = require('mongoose-unique-validator');
 
 const UserSchema = mongoose.Schema({
-  createdAt              : { type: Date  },
-  updatedAt              : { type: Date  },
-  password               : { type: String, select: false },
   username               : { type: String, required: true, unique: true },
+  password               : { type: String, select: false },
   recipesInCart          : { type: Array },
   arrayOfFavoriteRecipes : { type: Array }, /* --> an array of recipeIds */
+  createdAt              : { type: Date, default: Date.now  },
+  updatedAt              : { type: Date  },
 });
 
 // Make sure that only 1 user can exist with the same username
-UserSchema.plugin(uniqueValidator);
+UserSchema.plugin(uniqueValidator, { message: 'Error, expected {PATH} to be unique.' });
 
 // Defines the callback with a regular function to avoid problems with this schema
 UserSchema.pre('save', function (next) {
-  // SET createdAt AND updatedAt
-  const now = new Date();
-  this.updatedAt = now;
-  if (!this.createdAt) {
-    this.createdAt = now;
-  }
+  this.updatedAt = Date.now;
   // ENCRYPT PASSWORD
   const user = this;
-  if (!user.isModified('password')) {
-    return next();
-  }
-  bcrypt.genSalt(10, (err, salt) => {
-    bcrypt.hash(user.password, salt, (errHashing, hash) => {
-      if (errHashing) return next(errHashing);
-      user.password = hash;
-      next();
-    });
+  // if (!user.isModified('password')) {
+  //   return next();
+  // }
+  // bcrypt.hash(plaintext_password, saltRounds, function(err, hash) {}
+  bcrypt.hash(user.password, 10, (errHashing, hash) => {
+    if (errHashing) return next(errHashing);
+    user.password = hash;
+    next();
   });
 });
 
-UserSchema.methods.comparePassword = function (password, done) {
-  bcrypt.compare(password, this.password, (err, isMatch) => {
-    done(err, isMatch);
-  });
+UserSchema.statics.authenticate = function (email, password, next) {
+  User
+    .findOne({
+      email,
+    })
+    .exec((err, user) => {
+      if (err) return next(err);
+      if (!user) {
+        const error = new Error('User not found.');
+        error.status = 401;
+        return next(error);
+      }
+      if (user) {
+        // User found, compare password
+        bcrypt.compare(password, user.password, (error, isMatch) => {
+          if (error) return next(error);
+          if (isMatch === true) {
+            return next(null, user);
+          }
+          return next();
+        });
+      }
+    });
 };
 
-module.exports = mongoose.model('User', UserSchema);
+// UserSchema.methods.comparePassword = function (password, done) {
+//   bcrypt.compare(password, this.password, (err, isMatch) => {
+//     done(err, isMatch);
+//   });
+// };
+
+// module.exports = mongoose.model('User', UserSchema);
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
